5265d8fa49ef66436ca542e97b54352ecaa32b04
tomaspopik <tpopik1999@gmail.com>
2022-09-30 21:49:19 +0200

    feat: add event listener for NameInput

--- components/name-input/name-input.ts
+++ components/name-input/name-input.ts
@@ -4,6 +4,16 @@ export class NameInput extends Component {
 
     constructor(private inputElement: HTMLInputElement) {
         super(inputElement);
+
+        inputElement.addEventListener('input', this.changeInput)
+    }
+
+    private changeInput = (): void => {
+        this.mediator?.notify(this, 'input')
+    }
+
+    public getValue(): string {
+        return this.inputElement.value;
     }
 
 }
