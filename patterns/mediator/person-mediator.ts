6cb34479ffb6ea78755bba6bc0595f495c63654f
tomaspopik <tpopik1999@gmail.com>
2022-09-30 21:48:52 +0200

    feat: add action on FormDocument

--- patterns/mediator/person-mediator.ts
+++ patterns/mediator/person-mediator.ts
@@ -3,6 +3,8 @@ import {NameInput} from "../../components/name-input/name-input.js";
 import {SurnameInput} from "../../components/surname-input/surname-input.js";
 import {MailInput} from "../../components/mail-input/mail-input.js";
 import {PhoneNumberInput} from "../../components/phone-number-input/phone-number-input.js";
+import {FormDocument} from "../shared/FormDocument.js";
+import {Component} from "../../components/common/component.js";
 
 export class PersonMediator implements Mediator {
     private formDocument: FormDocument;
@@ -19,7 +21,10 @@ export class PersonMediator implements Mediator {
         this.phoneNumberInput.setMediator(this);
     }
 
-    public notify(sender: object, event: string): void {
+    public notify(sender: Component, event: string): void {
+        if (sender instanceof NameInput && event === 'input') {
+            this.formDocument.setName(sender.getValue());
+        }
 
     }
 
