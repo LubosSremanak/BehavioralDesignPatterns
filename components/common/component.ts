05d253612a1df067ae4f8dca6998cd8e0b6dd48c
tomaspopik <tpopik1999@gmail.com>
2022-09-30 17:04:01 +0200

    fix: add mediator into abstract Component class

--- components/common/component.ts
+++ components/common/component.ts
@@ -1,4 +1,14 @@
+import {Mediator} from "../../patterns/mediator/mediator.js";
+
 export abstract class Component {
+    private element?: HTMLElement;
+    protected mediator?: Mediator;
+
     protected constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    public setMediator(mediator: Mediator): void {
+        this.mediator = mediator;
     }
 }
