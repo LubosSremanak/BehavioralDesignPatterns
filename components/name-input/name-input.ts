2d0434d75cd28a8deac90c250f81ea3c5bcab115
tomaspopik <tpopik1999@gmail.com>
2022-09-30 17:04:44 +0200

    feat: add new components

---
+++ components/name-input/name-input.ts
@@ -0,0 +1,9 @@
+import {Component} from "../common/component.js";
+
+export class NameInput extends Component {
+
+    constructor(private inputElement: HTMLInputElement) {
+        super(inputElement);
+    }
+
+}
---
+++ components/phone-number-input/phone-number-input.ts
@@ -0,0 +1,9 @@
+import {Component} from "../common/component.js";
+
+export class PhoneNumberInput extends Component {
+
+    constructor(private inputElement: HTMLInputElement) {
+        super(inputElement);
+    }
+
+}
